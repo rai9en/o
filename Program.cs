1a441a15ade92febcb558b326aae406f35513a69
rai9en <helilovtdd@gmail.com>
2023-12-19 22:42:25 +0400

    ?

--- Program.cs
+++ Program.cs
@@ -16,6 +16,12 @@ namespace ConsoleApp10
             int n = arr.Length;
             for (int i = 0; i < n - 1; i++)
             {
+                if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         
             }
         }
