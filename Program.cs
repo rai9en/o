48655f2a2b3bae68e31c4b9ef727c6c6de77cb92
rai9en <helilovtdd@gmail.com>
2023-12-19 21:17:26 +0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public void BubbleSort(int[] arr)
+        {
+
+        }
+    }
+}
